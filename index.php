2cbfd6bdba822998b2b80ba52be23d25d72c7304
Abuzar Sial <abuzar.sial24@gmail.com>
2026-05-10 23:36:09 +0500

    [POS-11] Add QuickPOS logo to header

---
+++ index.php
@@ -0,0 +1,8 @@
+<div class="logo">QuickPOS</div>
+
+<nav>
+    <a href="#home">Home</a>
+    <a href="#features">Features</a>
+    <a href="#pricing">Pricing</a>
+    <a href="#contact">Contact</a>
+</nav>
